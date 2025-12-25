3009d34c24444b3ec28d7c1738410409103c4349
MAT Angel <matangel0494@gmail.com>
2025-12-25 17:19:20 -0600

    added Controlling navbar

--- src/app/components/smart-control-nabvar/smart-control-nabvar.component.ts
+++ src/app/components/smart-control-nabvar/smart-control-nabvar.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { ProfileInfoComponent } from "@shared/profile-info/profile-info.component";
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
 import { routes } from '../../app.routes';
+import { ProfileInfoComponent } from '@shared/profile-info/profile-info.component';
 
 @Component({
   selector: 'smart-control-navbar',
@@ -10,14 +10,22 @@ import { routes } from '../../app.routes';
   templateUrl: './smart-control-nabvar.component.html',
 })
 export class SmartControlNabvarComponent {
+  router = inject(Router);
   baseUrl = environment.gitRawUrl;
+  private readonly routesWithoutNavbar = ['/auth/login', '/'];
+
+  showNavbar(): boolean {
+    console.log(this.router.url);
+    return !this.routesWithoutNavbar.includes(this.router.url);
+  }
 
   routes = routes
-  .map((route) => ({
-    title: route.title ?? '',
-    path: route.path ?? ''
-  }))
-  .filter((route) => route && route.path && route.title);  //* Excluye laas rutas que no tengan path
+    .map((route) => ({
+      title: route.title ?? '',
+      path: route.path ?? ''
+    }))
+    .filter((route) => route && route.path && route.title);  //* Excluye las rutas que no tengan path
+
 
 }
 
