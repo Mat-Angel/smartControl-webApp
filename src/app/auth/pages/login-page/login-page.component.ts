7225891f5e3ce5cd38aa51a514582e576747f861
MAT Angel <matangel0494@gmail.com>
2025-12-27 14:44:01 -0600

    change navigation after login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -5,7 +5,8 @@ import { FormUtils } from '../../../utils/form-utils';
 import { EncryptionService } from '../../services/encryption.service';
 import { AlertService } from '@shared/alert-message/alert.service';
 import { AuthService } from '../../services/auth.service';
-import { NavigationService } from '@services/navigation.service';
+import { LoadingScreenService } from '@shared/loading-screen/loading-screen.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -14,10 +15,11 @@ import { NavigationService } from '@services/navigation.service';
 })
 export class LoginPageComponent {
   private alertService = inject(AlertService);
+  private loadingScreenService = inject(LoadingScreenService);
   encryptionService = inject(EncryptionService);
   authService = inject(AuthService);
   fb = inject(FormBuilder);
-  private navigationService = inject(NavigationService);
+  private router = inject(Router);
   formUtils = FormUtils;
 
   loginForm: FormGroup = this.fb.group({
@@ -30,15 +32,17 @@ export class LoginPageComponent {
     if (this.loginForm.invalid) {
       this.alertService.showAlert('Datos ingresados incorrectos')
     }
+    this.loadingScreenService.setLoadingState(true)
 
     const { email = '', password = '' } = this.loginForm.value;
     this.authService.login(email, password).
-      then(() => {
+    then(() => {
+        this.loadingScreenService.setLoadingState(false)
         console.log("Inicio de sesion exitoso");
         this.alertService.showAlert('Inicio de sesion exitoso', 'success');
-        this.navigationService.redirectToOrigin();
-      })
+        this.router.navigate(['/movements']);      })
       .catch((err) => {
+        this.loadingScreenService.setLoadingState(false)
         console.error("Error en Inicio de sesion:") //, err)
         this.alertService.showAlert('Error al iniciar sesion: Verifique email/contraseña', 'error');
     });
