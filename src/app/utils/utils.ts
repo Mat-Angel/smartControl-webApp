8c4cc3dd53c91e25edd7221f8b8a0b1ffefa5d45
MAT Angel <matangel0494@gmail.com>
2026-01-09 00:15:12 -0600

    Create image access parameterization

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -4,6 +4,24 @@ export class Utils {
   static currentDate = formatDate(new Date(), 'yyyy-MM-dd', 'en-US');
   static currentDateTime = formatDate(new Date(), 'yyyy-MM-dd HH:mm:ss', 'en-US');
 
+  static paths = {
+    'PNG': '/assets/PNG/',
+    'SVG': '/assets/SVG/',
+    'ICON': '/assets/ICONS/'
+  };
+
+  static pngImage = {
+    'REPORTBANNER': 'report_banner.png',
+    'MATBLACKLOGO': 'mat_minimal_black_logo.png',
+    'SMARTCONTROLLOGO': 'smartControl_logo.png'
+  };
+
+  static svgImage = {
+    'MATWHITEICON': 'mat_white_icon.svg'
+  };
+
+  static svgLogo = {
+  };
 
 }
 
