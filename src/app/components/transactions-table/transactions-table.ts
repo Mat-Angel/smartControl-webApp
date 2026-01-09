c035351ed8948b75b49b9c9bc4cd6e0fc706a757
MAT Angel <matangel0494@gmail.com>
2026-01-09 16:36:53 -0600

    Optimize generate report code

--- src/app/components/transactions-table/transactions-table.ts
+++ src/app/components/transactions-table/transactions-table.ts
@@ -10,12 +10,10 @@ import autoTable from 'jspdf-autotable';
 import { FormUtils } from '../../utils/form-utils';
 import { AlertService } from '@shared/alert-message/alert.service';
 
-
 export interface TwoColumnInfo {
   title: string;
   items: { title: string, description: string }[];
 }
-
 
 @Component({
   selector: 'transactions-table',
@@ -33,7 +31,6 @@ export class TransactionsTable {
   isEmpty = input<boolean>(false);
   deleteMovement = output<string>();
 
-
   monthMap = signal({
     '=0': 'actual',
     '=1': 'anterior',
@@ -42,9 +39,7 @@ export class TransactionsTable {
   })
 
   selectedMovement = signal<Transactions | null>(null);
-
   pageSize = signal(10);
-
   currentPage = signal(1);
 
   totalItems = computed(() => {
@@ -74,6 +69,74 @@ export class TransactionsTable {
     this.movementDetailModal()?.nativeElement.close();
   }
 
+  generateReportPdf() {
+    if (this.transactionsList().length === 0) { return };
+
+    // Set document data
+    const totals = this.transactionsList().reduce(
+      (acc, t) => {
+        const amount = Number(t.amount) || 0;
+        acc.balance += amount;
+        if (t.transactionType === 'income') {
+          acc.income += amount;
+        } else if (t.transactionType === 'outgoing') {
+          acc.expenses += amount;
+        }
+        return acc;
+      },
+      {
+        balance: 0,
+        income: 0,
+        expenses: 0,
+      }
+    );
+
+    const movementsDataIndividual = {
+      title: 'Reporte de movimientos',
+      items: [
+        {
+          title: 'Saldo disponible',
+          description: `$ ${totals.balance}`
+        },
+        {
+          title: 'Total de ingresos',
+          description: `$ ${totals.income}`
+        },
+        {
+          title: 'Total de gastos',
+          description: `$ ${totals.expenses}`
+        },
+      ]
+    };
+
+    const movementItems = movementsDataIndividual.items;
+
+    const tableData = {
+      headValues: [['Nombre', 'Descripción', 'Monto', 'Información de pago', 'Tipo de transacción', 'Categoría', 'Fecha de operación']],
+      tableValues:
+        this.transactionsList().map(t => [
+          t.title,
+          t.description,
+          `$${t.amount}`,
+          `${t.paymentInfo.bankName} ${t.paymentInfo.title}`,
+          FormUtils.transactionTypeTranslations[t.transactionType],
+          t.category,
+          t.operationDate,
+        ])
+    }
+
+    Promise.all([
+      this.loadImage(Utils.getPngImage('REPORT_BANNER')),
+      this.loadImage(Utils.getPngImage('SMARTCONTROL_LOGO')),
+      this.loadImage(Utils.getPngImage('MAT_BLACK_LOGO'))
+    ])
+      .then(([banner, SCLogo, matLogo]) => {
+        // 3️⃣ render PDF (TODO aquí)
+        this.renderPdf(banner, SCLogo, matLogo, movementsDataIndividual, tableData);
+        this.alertService.showAlert('Se ha generado el reporte exitosamente.', 'success');
+      });
+
+  }
 
   loadImage(src: string): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
@@ -85,83 +148,36 @@ export class TransactionsTable {
     });
   }
 
-
-  generateReportPdf() {
-    if (this.transactionsList().length === 0) { return };
-
+  private renderPdf(banner: HTMLImageElement, SCLogo: HTMLImageElement, matLogo: HTMLImageElement, movementsDataIndividual: any, tableData: any) {
     const doc = new jsPDF();
 
-    const marginLeft = 20;
-    const marginRight = 20;
-    const marginBottom = 18;
-    const pageWidth = doc.internal.pageSize.getWidth();
-    const pageHeight = doc.internal.pageSize.getHeight();
-
-    const totalBalance = this.transactionsList().reduce((sum, t) => sum + Number(t.amount), 0);
-    const totalIncome = this.transactionsList().filter(t => t.transactionType === 'income')
-      .reduce((sum, t) => sum + Number(t.amount), 0);
-    const totalExpenses = this.transactionsList().filter(t => t.transactionType === 'outgoing')
-      .reduce((sum, t) => sum + Number(t.amount), 0);
-
-
-    const movementsDataIndividual = {
-      title: 'Reporte de movimientos',
-      items: [
-        {
-          title: 'Saldo disponible',
-          description: `$ ${totalBalance}`
-        },
-        {
-          title: 'Total de ingresos',
-          description: `$ ${totalIncome}`
-        },
-        {
-          title: 'Total de gastos',
-          description: `$ ${totalExpenses}`
-        },
-      ]
-    };
-
-    const data = movementsDataIndividual.items;
-
-    const headValues = [['Nombre', 'Descripción', 'Monto', 'Información de pago', 'Tipo de transacción', 'Categoría', 'Fecha de operación']];
-    const tableValues =
-      this.transactionsList().map(t => [
-        t.title,
-        t.description,
-        `$${t.amount}`,
-        `${t.paymentInfo.bankName} ${t.paymentInfo.title}`,
-        FormUtils.transactionTypeTranslations[t.transactionType],
-        t.category,
-        t.operationDate,
-      ]);
-
-
-
+    const movementItems = movementsDataIndividual.items;
     const date = new Date();
     date.setMonth(date.getMonth() - this.monthOffset());
     const month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
 
-    const imgBanner = new Image();
-    imgBanner.src = Utils.getPngImage('REPORT_BANNER');
-    doc.addImage(imgBanner, 'png', 0, 0, 210, 35); //TODO Resolver el obtener de forma automatica los tamaños de imagen
+    // Document configs
+    const margins = { marginLeft: 15, marginRight: 25, bottom: 36 } as const;
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+    const docConfigs = { margins, pageWidth, pageHeight };
 
-    const imgSCLogo = new Image();
-    imgSCLogo.src = Utils.getPngImage('SMARTCONTROL_LOGO');
-    doc.addImage(imgSCLogo, 'PNG', 10, 7, 563 * .1, 188 * .1); //TODO Resolver el obtener de forma automatica los tamaños de imagen
-
-    const imgMatLogo = new Image();
-    imgMatLogo.src = Utils.getPngImage('MAT_BLACK_LOGO');
-    doc.addImage(imgMatLogo, 'png', 150, 7, 502 * .09, 192 * .09); //TODO Resolver el obtener de forma automatica los tamaños de imagen
+    // HEADER
+    const bannerProps = doc.getImageProperties(banner);
+    const SCLogoProps = doc.getImageProperties(SCLogo);
+    const matLogoProps = doc.getImageProperties(matLogo);
+    doc.addImage(banner, 'png', 0, 0, docConfigs.pageWidth, bannerProps.height / docConfigs.pageHeight * 100);
+    doc.addImage(SCLogo, 'PNG', 10, 7, SCLogoProps.width * .1, SCLogoProps.height * .1);
+    doc.addImage(matLogo, 'png', 150, 7, matLogoProps.width * .09, matLogoProps.height * .09);
 
     doc.setFont('helvetica', 'bold');
     doc.setFontSize(13);
-    doc.text('www.mat-angel.com', 150, 30);
+    doc.text('www.mat-angel.com', 150, 29);
 
     doc.link(150, 7, 502 * .09, (192 * .09) + 5, { url: 'https://mat-angel.com' });
 
     doc.setFontSize(18);
-    doc.text(movementsDataIndividual.title, pageWidth / 2, 45, { align: 'center' });
+    doc.text(movementsDataIndividual.title, docConfigs.pageWidth / 2, 45, { align: 'center' });
 
     doc.setLineWidth(0.5);
     doc.line(15, 50, 195, 50);
@@ -174,128 +190,54 @@ export class TransactionsTable {
     doc.setFontSize(14);
     doc.text(`Periodo: ${month}`, 15, 70, { align: 'left' });
 
-
     doc.setFontSize(12);
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < movementItems.length; i++) {
       doc.setFont('helvetica', 'bold');
-      doc.text(`${data[i].title}:`, 15, 80 + i * 10, {
+      doc.text(`${movementItems[i].title}:`, 15, 80 + i * 10, {
         align: 'left'
       });
       doc.setFont('helvetica', 'normal');
-      doc.text(`${data[i].description ?? 'N/A'} `, 70, 80 + i * 10, {
+      doc.text(`${movementItems[i].description ?? 'N/A'} `, 70, 80 + i * 10, {
         align: 'left'
       });
-
     }
 
+    this.generatePdfTable(doc, tableData.headValues, tableData.tableValues, docConfigs);
+
+    //Abre pdf en una nueva pestaña
+    window.open(doc.output('bloburl'), '_blank');
+
+    //Guarda archivo pdf
+    doc.save(`Reporte_${month}_${date.getFullYear()}.pdf`);
+
+  }
+
+  generatePdfTable(doc: jsPDF, tableHeaders: any, bodyTable: any, docConfigs?: any) {
     autoTable(doc, {
-      margin: { left: marginLeft - 5, right: marginRight - 5, bottom: marginBottom + 15 },
-      startY: 120,
-      head: headValues,
-      body: tableValues,
+      margin: docConfigs.margins,
+      startY: 110,
+      head: tableHeaders,
+      body: bodyTable,
       didDrawPage: () => {
         const pageNumber = doc.getCurrentPageInfo().pageNumber;
         const disclaimerFooterText =
           'Este documento es una representación visual sin valor jurídico, fiscal ni administrativo. Ha sido creado exclusivamente como material de ejemplo/demostración. La información contenida es simulada y no vinculante. Queda prohibido su uso como comprobante de transacción, identidad o propiedad.';
         const copyrightFooterText =
           '© 2026 MAT-ANGEL. Queda prohibida la reproducción total o parcial de este modelo de documento para fines ajenos a esta demostración.';
-        const maxWidth = pageWidth * 1.3;
+        const maxWidth = docConfigs.pageWidth * 1.3;
         const disclaimerLines = doc.splitTextToSize(disclaimerFooterText, maxWidth);
         const copyrightLines = doc.splitTextToSize(copyrightFooterText, maxWidth);
 
         doc.setFont('helvetica', 'normal');
         doc.setFontSize(8);
         doc.setTextColor(120);
-        doc.text(disclaimerLines, marginLeft, pageHeight - 24); // o pageHeight - 12 si son 2+ líneas
-        doc.text(copyrightLines, marginLeft, pageHeight - 12); // o pageHeight - 12 si son 2+ líneas
+        doc.text(disclaimerLines, docConfigs.margins.marginLeft, docConfigs.pageHeight - 24); // o pageHeight - 12 si son 2+ líneas
+        doc.text(copyrightLines, docConfigs.margins.marginLeft, docConfigs.pageHeight - 12); // o pageHeight - 12 si son 2+ líneas
 
         //Número de página
-        doc.text(`Página ${pageNumber}`, pageWidth - 30, pageHeight - 5);
+        doc.text(`Página ${pageNumber}`, docConfigs.pageWidth - docConfigs.margins.marginRight, docConfigs.pageHeight - 5);
       },
     });
-
-    doc.save(`Reporte_${month}_${date.getFullYear()}.pdf`);
-    this.alertService.showAlert('Se ha generado el reporte exitosamente.', 'success');
-
-    const pdfUrl = doc.output('bloburl');
-    window.open(pdfUrl, '_blank');
   }
-
-  /*
-    generatePdf(data: Transactions[]) {
-      // 1️⃣ preparar datos síncronos
-      const tableRows = data.map(item => [
-        item.date,
-        item.description,
-        `$${item.amount.toFixed(2)}`
-      ]);
-
-      const footerText =
-        '© 2026 MAT-ANGEL. Uso exclusivo demostrativo.';
-
-      // 2️⃣ cargar imágenes (asíncrono)
-      Promise.all([
-        this.loadImage('assets/PNG/logocolorsmall.png'),
-        this.loadImage('assets/PNG/header.png'),
-      ])
-        .then(([logo, header]) => {
-          // 3️⃣ render PDF (TODO aquí)
-          this.renderPdf({
-            logo,
-            header,
-            tableRows,
-            footerText
-          });
-        });
-    }
-
-    private renderPdf(params: {
-      logo: HTMLImageElement;
-      header: HTMLImageElement;
-      tableRows: any[][];
-      footerText: string;
-    }) {
-      const doc = new jsPDF('p', 'mm', 'a4');
-
-      // HEADER
-      doc.addImage(params.header, 'PNG', 0, 0, 210, 30);
-      doc.addImage(params.logo, 'PNG', 20, 10, 40, 12);
-
-      doc.setFontSize(12);
-      doc.text('Estado de Cuenta', 20, 45);
-
-      // TABLA
-      autoTable(doc, {
-        startY: 50,
-        head: [['Fecha', 'Concepto', 'Monto']],
-        body: params.tableRows,
-        margin: { bottom: 20 },
-
-        didDrawPage: () => {
-          const pageHeight = doc.internal.pageSize.getHeight();
-          const pageWidth = doc.internal.pageSize.getWidth();
-
-          const maxWidth = pageWidth - 40;
-          const lines = doc.splitTextToSize(params.footerText, maxWidth);
-
-          doc.setFontSize(8);
-          doc.setTextColor(120);
-
-          doc.text(lines, 20, pageHeight - 10);
-          doc.text(
-            `Página ${doc.getCurrentPageInfo().pageNumber}`,
-            pageWidth - 30,
-            pageHeight - 10
-          );
-        }
-      });
-
-      doc.save('estado-cuenta.pdf');
-    }
-
-
-    */
-
-
 
 }
