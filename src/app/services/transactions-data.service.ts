57995b69e700eac4aeb79814e98bc0fe6e971e41
MAT Angel <matangel0494@gmail.com>
2026-01-31 19:18:16 -0600

    Add mem cache suport

--- src/app/services/transactions-data.service.ts
+++ src/app/services/transactions-data.service.ts
@@ -1,4 +1,4 @@
-import { computed, inject, Injectable } from '@angular/core';
+import { computed, inject, Injectable, effect, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable, of, tap } from 'rxjs';
 import { Transactions } from '../interfaces/transactions.interface';
@@ -6,9 +6,14 @@ import { PaymentMethod } from '../interfaces/payment-methods.interface';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../auth/services/auth.service';
 
+interface FirebasePostResponse {
+  name: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class TransactionsDataService {
   private http = inject(HttpClient);
   private authService = inject(AuthService);
@@ -16,20 +21,30 @@ export class TransactionsDataService {
   private readonly _token = computed(() => this.authService.token());
   private readonly _userId = computed(() => this.authService.userId());
 
+  private _paymentMethods = signal<PaymentMethod[]>([]);
+  private _transactions = signal<Transactions[]>([]);
+  private _automatedPayments = signal<Transactions[]>([]);
+
   private readonly baseUrl = environment.firebaseUrl;
 
   loadTransactions(token: string, userId: string): Observable<Transactions[]> {
     if (!userId || !token) return of([]);
 
     const url = `${this.baseUrl}${userId}/smartControl/transactions.json?auth=${token}`;
-    return this.http.get<Record<string, Transactions> | null>(url).pipe(
-      // Transformar el objeto de Firebase a un array
-      map(resp => {
-        if (!resp) return [];
-        return Object.entries(resp).map(([id, data]) => ({ ...data, id: id, })) as Transactions[];   //"id" clave generada por Firebase
-      }),
-      //tap(resp => console.log('Transactions mapeadas:', resp))
-    );
+    if (this._transactions().length === 0) {
+      return this.http.get<Record<string, Transactions> | null>(url).pipe(
+        // Transformar el objeto de Firebase a un array
+        map(resp => {
+          if (!resp) return [];
+          return Object.entries(resp).map(([id, data]) => ({ ...data, id: id, })) as Transactions[];   //"id" clave generada por Firebase
+        }),
+        tap(resp => {
+          this._transactions.set(resp);
+        })
+      );
+
+    }
+    return of(this._transactions());
   }
 
 
@@ -37,14 +52,23 @@ export class TransactionsDataService {
     if (!userId || !token) return of([]);
 
     const url = `${this.baseUrl}${userId}/smartControl/automatedPayments.json?auth=${token}`;
-    return this.http.get<Record<string, Transactions> | null>(url).pipe(
-      // Transformar el objeto de Firebase a un array
-      map(resp => {
-        if (!resp) return [];
-        return Object.entries(resp).map(([id, data]) => ({ ...data, id: id, })) as Transactions[];   //"id" clave generada por Firebase
-      }),
-      //tap(resp => console.log('Transactions mapeadas:', resp))
-    );
+
+    if (this._automatedPayments().length === 0) {
+      return this.http.get<Record<string, Transactions> | null>(url).pipe(
+        // Transformar el objeto de Firebase a un array
+        map(resp => {
+          if (!resp) return [];
+          return Object.entries(resp).map(([id, data]) => ({ ...data, id: id, })) as Transactions[];   //"id" clave generada por Firebase
+        }),
+        tap(resp => {
+
+          this._automatedPayments.set(resp);
+
+        })
+      );
+    }
+
+    return of(this._automatedPayments());
   }
 
 
@@ -52,7 +76,6 @@ export class TransactionsDataService {
     if (!this._token() || !this._userId()) return of(null);
 
     return this.http.get<Transactions | null>(`${this.baseUrl}${this._userId()}/smartControl/transactions/${transactionId}.json?auth=${this._token()}`)
-      //.pipe(tap(resp => console.log('Transaction devuelta:', resp)));
   }
 
 
@@ -60,42 +83,68 @@ export class TransactionsDataService {
     if (!this._token() || !this._userId()) return of(null);
 
     return this.http.get<Transactions | null>(`${this.baseUrl}${this._userId()}/smartControl/automatedPayments/${transactionId}.json?auth=${this._token()}`)
-      //.pipe(tap(resp => console.log('Transaction devuelta:', resp)));
   }
 
 
-  saveTransaction(transaction: Transactions) {
-    if (!this._token() || !this._userId()) return of([]);
-
-    return this.http.post(`${this.baseUrl}${this._userId()}/smartControl/transactions.json?auth=${this._token()}`, transaction);
+  saveTransaction(transaction: Transactions): Observable<FirebasePostResponse> {
+    if (!this._token() || !this._userId()) return of();
+    return this.http.post<FirebasePostResponse>(`${this.baseUrl}${this._userId()}/smartControl/transactions.json?auth=${this._token()}`, transaction)
+      .pipe(
+        tap(resp => {
+          this._transactions.update(txs => [...txs, { ...transaction, id: resp.name }]
+            .sort((a, b) => b.operationDate.localeCompare(a.operationDate))
+          );
+        })
+      );
   }
 
 
-  saveAutomatedPayment(transaction: Transactions) {
-    if (!this._token() || !this._userId()) return of([]);
+  saveAutomatedPayment(transaction: Transactions): Observable<FirebasePostResponse> {
+    if (!this._token() || !this._userId()) return of();
 
-    return this.http.post(`${this.baseUrl}${this._userId()}/smartControl/automatedPayments.json?auth=${this._token()}`, transaction);
+    return this.http.post<FirebasePostResponse>(`${this.baseUrl}${this._userId()}/smartControl/automatedPayments.json?auth=${this._token()}`, transaction)
+      .pipe(
+        tap(resp => {
+          this._automatedPayments.update(txs => [...txs, { ...transaction, id: resp.name }]
+            .sort((a, b) => b.operationDate.localeCompare(a.operationDate))
+          );
+        })
+      );
   }
 
 
   updateTransaction(transaction: Transactions, transactionId: string) {
     if (!this._token() || !this._userId()) return of([]);
 
-    return this.http.put(`${this.baseUrl}${this._userId()}/smartControl/transactions/${transactionId}.json?auth=${this._token()}`, transaction);
+    return this.http.put(`${this.baseUrl}${this._userId()}/smartControl/transactions/${transactionId}.json?auth=${this._token()}`, transaction)
+      .pipe(
+        tap(resp => {
+          this._transactions.update(txs => txs.map(t => t.id === transactionId ? transaction : t));
+        })
+      );
   }
 
 
   updateAutomatedPayment(transaction: Transactions, transactionId: string) {
     if (!this._token() || !this._userId()) return of([]);
 
-    return this.http.put(`${this.baseUrl}${this._userId()}/smartControl/automatedPayments/${transactionId}.json?auth=${this._token()}`, transaction);
+    return this.http.put(`${this.baseUrl}${this._userId()}/smartControl/automatedPayments/${transactionId}.json?auth=${this._token()}`, transaction)
+      .pipe(
+        tap(resp => {
+          this._automatedPayments.update(txs => txs.map(t => t.id === transactionId ? { ...transaction, id: transactionId } : t));
+
+        })
+      );
   }
 
 
   deleteTransaction(id: string) {
     if (!this._token() || !this._userId()) return of([]);
 
-    return this.http.delete(`${this.baseUrl}${this._userId()}/smartControl/transactions/${id}.json?auth=${this._token()}`);
+    return this.http.delete(`${this.baseUrl}${this._userId()}/smartControl/transactions/${id}.json?auth=${this._token()}`)
+      .pipe(tap(() => {
+        this._transactions.update(tx => tx.filter(t => t.id !== id));
+      }));
   }
 
 
@@ -103,19 +152,33 @@ export class TransactionsDataService {
     if (!userId || !token) return of([]);
 
     const url = `${this.baseUrl}${userId}/smartControl/paymentMethods.json?auth=${token}`;
-    return this.http.get<Record<string, PaymentMethod> | null>(url).pipe(
-      // Transformar el objeto de Firebase a un array
-      map(resp => {
-        if (!resp) return [];
-        return Object.entries(resp).map(([id, data]) => ({ ...data, id: id, })) as PaymentMethod[];   //"id" clave generada por Firebase
-      }),
-      //tap(resp => console.log('Cards mapeadas:', resp))
-    );
+
+    if (this._paymentMethods().length === 0) {
+      return this.http.get<Record<string, PaymentMethod> | null>(url).pipe(
+        // Transformar el objeto de Firebase a un array
+        map(resp => {
+          if (!resp) return [];
+          return Object.entries(resp).map(([id, data]) => ({ ...data, id: id, })) as PaymentMethod[];   //"id" clave generada por Firebase
+        }),
+        tap(resp => {
+          this._paymentMethods.set(resp);
+        })
+      );
+    }
+    return of(this._paymentMethods());
   }
 
 
-  saveCard(transaction: PaymentMethod) {
-    if (!this._token() || !this._userId()) return of([]);
-    return this.http.post(`${this.baseUrl}${this._userId()}/smartControl/paymentMethods.json?auth=${this._token()}`, transaction);
+  saveCard(card: PaymentMethod): Observable<FirebasePostResponse> {
+    if (!this._token() || !this._userId()) return of();
+    return this.http.post<FirebasePostResponse>(`${this.baseUrl}${this._userId()}/smartControl/paymentMethods.json?auth=${this._token()}`, card)
+      .pipe(
+        tap(resp => {
+          this._paymentMethods.update(txs => [...txs, { ...card, id: resp.name }]
+            .sort((a, b) => a.accInfo.bankName.localeCompare(b.accInfo.bankName))
+          );
+        })
+      );
+
   }
 }
