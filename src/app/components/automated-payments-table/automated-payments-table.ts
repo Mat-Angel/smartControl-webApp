3e70b865fa85ab45e0609090dd226d66095dd2be
MAT Angel <matangel0494@gmail.com>
2026-02-21 18:50:33 -0600

    Add register charge from automated payments button

--- src/app/components/automated-payments-table/automated-payments-table.ts
+++ src/app/components/automated-payments-table/automated-payments-table.ts
@@ -1,10 +1,13 @@
 import { Component, ElementRef, inject, input, output, signal, viewChild } from '@angular/core';
-import { Transactions } from '@interfaces/transactions.interface';
+import { PaymentPlan, Transactions } from '@interfaces/transactions.interface';
 import { IconsService } from '../../services/icons.service';
-import { CurrencyPipe } from '@angular/common';
-import { RouterLink } from "@angular/router";
+import { CurrencyPipe, formatDate } from '@angular/common';
+import { Router, RouterLink } from "@angular/router";
 import { FormUtils } from '../../utils/form-utils';
 import { MovementDetailInfo } from "../movement-detail-info/movement-detail-info";
+import { Utils } from '../../utils/utils';
+import { TransactionsDataService } from '@services/transactions-data.service';
+import { AlertService } from '@shared/alert-message/alert.service';
 
 @Component({
   selector: 'automated-payments-table',
@@ -20,13 +23,18 @@ export class AutomatedPaymentsTable {
   deleteMovement = output<string>();
 
   iconsService = inject(IconsService);
+  private transactionsDataService = inject(TransactionsDataService);
+  private alertService = inject(AlertService);
+  private router = inject(Router);
+
   readonly formUtils = FormUtils;
+  readonly utils = Utils;
   selectedMovement = signal<Transactions | null>(null);
   monthOffset = signal<number>(0);
 
 
   onSelectedMovement(payment: Transactions) {
-    //console.log('transaction selected: ', { transaction });
+    // console.log('transaction selected: ', payment);
     this.selectedMovement.set(payment);
   }
 
@@ -35,5 +43,60 @@ export class AutomatedPaymentsTable {
     this.movementDetailModal()?.nativeElement.close();
   }
 
+  onRegisterCharge() {
+    const charge = this.selectedMovement();
+    if (charge) {
+      const partialAmount = (+charge.amount / charge.periodicTransaction!.installment!.total).toFixed(2);
+      const isInstallment = charge.periodicTransaction?.paymentPlanType === PaymentPlan.installment;
+
+      const data: Transactions = {
+        // id: '',
+        title: charge.title,
+        description: `${charge.description} ${isInstallment ? ` (Mensualidad ${this.getInstallmentNumber(charge.operationDate)} de ${charge.periodicTransaction?.installment.total})` : ''}`,
+        amount: isInstallment ? partialAmount.toString() : charge.amount,
+        transactionType: charge.transactionType,
+        category: charge.category,
+        operationDate: this.getNewOperationDate(charge.operationDate, charge.periodicTransaction?.every ?? undefined),
+
+        paymentInfo: { ...charge.paymentInfo }
+      }
+
+      this.transactionsDataService.saveTransaction(data).subscribe({
+        next: (response) => {
+          this.alertService.showAlert('Se ha guardado la transacción correctamente', 'success');
+          //console.log('OK:', response);
+          this.router.navigate(['/movements']);
+        },
+        error: (err) => {
+          this.alertService.showAlert('Hubo un problema al guardar la transacción. Intente de nuevo', 'error');
+          //console.error('Error:', err);
+        }
+      });
+    }
+  }
+
+  getInstallmentNumber = (operationDate: string): number => {
+    const currendDate = new Date();
+    const date = new Date(operationDate);
+
+    currendDate.setDate(1);
+    date.setDate(1);
+
+    let installmentNumber = (currendDate.getFullYear() - date.getFullYear()) * 12;
+    installmentNumber += currendDate.getMonth() - date.getMonth();
+
+    return installmentNumber + 1;
+  };
+
+  getNewOperationDate = (operationDate: string, cutDay?: number) => {
+    const currentDate = new Date();
+    const day = cutDay ? cutDay : +operationDate.split('-')[2];
+
+    currentDate.setDate(day);
+
+    const date = formatDate(currentDate, 'yyyy-MM-dd', 'en-US');
+    return date;
+  };
 
 }
+
